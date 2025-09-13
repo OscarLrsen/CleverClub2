23e10b37c5e1d3818a9ffa12f1e4d497e5281614
Ricky Ilic <ricky.ilic@edu.newton.se>
2025-09-13 22:34:17 +0200

    Admin panel skapad (frontend)

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,34 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
 import AboutUsPage from "./pages/AboutUsPage";
+import AdminPanel from "./pages/AdminPanel";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
+        {/* Vanliga sidor med navbar */}
+        <Route
+          path="/*"
+          element={
+            <>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/leaderboard" element={<LeaderboardPage />} />
+                <Route path="/about" element={<AboutUsPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+              </Routes>
+            </>
+          }
+        />
+
+        {/* Admin-sida utan navbar */}
+        <Route path="/admin" element={<AdminPanel />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
     </Router>
   );
